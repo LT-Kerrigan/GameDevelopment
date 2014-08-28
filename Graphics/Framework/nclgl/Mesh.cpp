a4673531f075e09e15890d739fd3755a92c2854d
Kerrigan <anavmihut@gmail.com>
2014-08-28 23:22:29 +0100

    Implemented OBJMesh and ChildMeshInterface.

--- Graphics/Framework/nclgl/Mesh.cpp
+++ Graphics/Framework/nclgl/Mesh.cpp
@@ -58,12 +58,14 @@ Mesh* Mesh::GenerateQuad() {
 	m->type = GL_TRIANGLE_STRIP;
 
 	m->vertices = new Vector3[m->numVertices];
+	m->textureCoords = new Vector2[m->numVertices];
+
 	m->vertices[0] = Vector3(-1.0f, -1.0f, 0.0f);
 	m->vertices[1] = Vector3(-1.0f, 1.0f, 0.0f);
 	m->vertices[2] = Vector3(1.0f, -1.0f, 0.0f);
 	m->vertices[3] = Vector3(1.0f, 1.0f, 0.0f);
 
-	m->textureCoords = new Vector2[m->numVertices];
+	
 	m->textureCoords[0] = Vector2(0.0f, 1.0f);
 	m->textureCoords[1] = Vector2(0.0f, 0.0f);
 	m->textureCoords[2] = Vector2(1.0f, 1.0f);
