bdbedabe1f545a06a0b19f5dabb8f58a07f56e6e
Kerrigan <anavmihut@gmail.com>
2014-08-31 22:54:40 +0100

    Real-time lighting test 2B

---
+++ Graphics/Framework/Graphics For Games/12) Per Fragment Lighting B/Renderer.h
@@ -0,0 +1,40 @@
+/*****************************************************************************************
+* 	         .--.              															 *
+* ::\`--._,'.::.`._.--'/::     @author Ana M. Mihut		@course  Graphics				 *
+* ::::.  ` __::__ '  .::::     @alias  LT-Kerrigan		@link    cs.ncl.ac.uk			 *
+* ::::::-:.`'..`'.:-::::::     @date   23.03.2014       @origin  Newcastle 				 *
+* ::::::::\ `--' /::::::::     @title  Tutorial #14		@details CubeMap			 	 *
+*																						 *
+*****************************************************************************************/
+
+#pragma once
+
+#include "../../nclgl/OGLRenderer.h"
+#include "../../nclgl/Camera.h"
+#include "../../nclgl/HeightMap.h"
+
+class Renderer : public OGLRenderer {
+public:
+	Renderer(Window &parent);
+	virtual ~Renderer(void);
+
+	virtual void RenderScene();
+	virtual void UpdateScene(float msec);
+
+protected:
+	void DrawHeightmap();
+	void DrawWater();
+	void DrawSkybox();
+
+	Shader*    lightShader;
+	Shader*    reflectShader;
+	Shader*    skyboxShader;
+
+	HeightMap* heightMap;
+	Mesh*      quad;
+	Light*     light;
+	Camera*    camera;
+
+	GLuint     cubeMap;
+	float      waterRotate;
+};
