63f802191d184e25c5981328a2479cf2e79cc56d
Kerrigan <anavmihut@gmail.com>
2014-09-11 01:05:14 +0100

    done

--- Graphics/Framework/Graphics For Games/12) Per Fragment Lighting B/Renderer.cpp
+++ Graphics/Framework/Graphics For Games/12) Per Fragment Lighting B/Renderer.cpp
@@ -3,7 +3,7 @@
 * ::\`--._,'.::.`._.--'/::     @author Ana M. Mihut		@course  Graphics				 *
 * ::::.  ` __::__ '  .::::     @alias  LT-Kerrigan		@link    cs.ncl.ac.uk			 *
 * ::::::-:.`'..`'.:-::::::     @date   23.03.2014       @origin  Newcastle 				 *
-* ::::::::\ `--' /::::::::     @title  Tutorial #14		@details CubeMap			 	 *
+* ::::::::\ `--' /::::::::     @title  Tutorial #13		@details CubeMap			 	 *
 *																						 *
 *****************************************************************************************/
 
@@ -11,7 +11,7 @@
 
 Renderer::Renderer(Window &parent) : OGLRenderer(parent) {
 	camera    = new Camera();
-	heightMap = new HeightMap("../../Textures/terrain.raw");
+	heightMap = new HeightMap("../../Textures/HeightMapMedium.raw");
 	quad      = Mesh::GenerateQuad();
 
 	camera->SetPosition(Vector3(RAW_WIDTH * HEIGHTMAP_X / 2.0f, 500.0f, RAW_WIDTH * HEIGHTMAP_X));
@@ -20,7 +20,7 @@ Renderer::Renderer(Window &parent) : OGLRenderer(parent) {
 
 	reflectShader = new Shader("../../Shaders/PerPixelVertex.glsl", "../../Shaders/ReflectFragment.glsl");
 	skyboxShader  = new Shader("../../Shaders/SkyboxVertex.glsl", "../../Shaders/SkyboxFragment.glsl");
-	lightShader   = new Shader("../../Shaders/PerPixelVertex.glsl", "../../Shaders/PerPixelFragment.glsl");
+	lightShader   = new Shader("../../Shaders/PerPixelVertex.glsl", "../../Shaders/MultiTexPerPixelFragment.glsl");
 
 	if (!reflectShader->LinkProgram() || !lightShader->LinkProgram() || !skyboxShader->LinkProgram()) {
 		
@@ -30,8 +30,8 @@ Renderer::Renderer(Window &parent) : OGLRenderer(parent) {
 	quad->SetTexture(SOIL_load_OGL_texture("../../Textures/water.tga", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
 
 	heightMap->SetTexture(SOIL_load_OGL_texture("../../Textures/Barren Reds.JPG", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
-
-	heightMap->SetBumpMap(SOIL_load_OGL_texture("../../Textures/Barren RedsDOT3.JPG", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
+	heightMap->SetBumpMap(SOIL_load_OGL_texture("../../Textures/grass_tiled.PNG", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
+	//heightMap->SetBumpMap(SOIL_load_OGL_texture("../../Textures/Barren RedsDOT3.JPG", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
 
 	cubeMap = SOIL_load_OGL_cubemap("../../Textures/rusted_west.jpg", "../../Textures/rusted_east.jpg", 
 		                            "../../Textures/rusted_up.jpg", "../../Textures/rusted_down.jpg",
@@ -99,9 +99,10 @@ void Renderer::DrawHeightmap(){
 	SetShaderLight(*light);
 
 	glUniform3fv(glGetUniformLocation(currentShader->GetProgram(), "cameraPos"), 1, (float*)&camera->GetPosition());
-
+	
 	glUniform1i(glGetUniformLocation(currentShader->GetProgram(), "diffuseTex"), 0);
-	glUniform1i(glGetUniformLocation(currentShader->GetProgram(), "bumpTex"), 1);
+	//glUniform1i(glGetUniformLocation(currentShader->GetProgram(), "bumpTex"), 1);
+	glUniform1i(glGetUniformLocation(currentShader->GetProgram(), "diffuseGrassTex"), 1);
 
 	modelMatrix.ToIdentity();
 	textureMatrix.ToIdentity();
@@ -126,7 +127,7 @@ void Renderer::DrawWater(){
 	glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMap);
 
 	float heightX = (RAW_WIDTH * HEIGHTMAP_X / 2.0f);
-	float heightY = 256 * HEIGHTMAP_Y / 3.0f;
+	float heightY = 256 * HEIGHTMAP_Y / 10.0f;//3.0f;
 	float heightZ = (RAW_HEIGHT * HEIGHTMAP_Z / 2.0f);
 
 	modelMatrix = Matrix4::Translation(Vector3(heightX, heightY, heightZ)) * Matrix4::Scale(Vector3(heightX, 1, heightZ)) * Matrix4::Rotation(90, Vector3(1.0f, 0.0f, 0.0f));
