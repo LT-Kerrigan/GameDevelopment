96fcbd47249a44b02fe7546e8ab15afb845b839a
Kerrigan <anavmihut@gmail.com>
2015-02-03 23:55:16 +0000

    Refactored Test_12.

---
+++ Graphics/Framework/TestScenes/12_PerFragmentLightingB/Renderer.h
@@ -0,0 +1,43 @@
+/*****************************************************************************************
+* 	         .--.              															 *
+* ::\`--._,'.::.`._.--'/::     @author Ana M. Mihut		@course  Graphics				 *
+* ::::.  ` __::__ '  .::::     @alias  LT-Kerrigan		@link    cs.ncl.ac.uk			 *
+* ::::::-:.`'..`'.:-::::::     @date   23.03.2014       @origin  Newcastle 				 *
+* ::::::::\ `--' /::::::::     @title  Tutorial #14		@details CubeMap			 	 *
+*																						 *
+*****************************************************************************************/
+
+#pragma once
+
+#include "../../GraphicsFramework/OGLRenderer.h"
+#include "../../GraphicsFramework/Camera.h"
+#include "../../GraphicsFramework/HeightMap.h"
+
+class Renderer : public OGLRenderer {
+public:
+	Renderer(Window &parent);
+	virtual ~Renderer(void);
+
+	virtual void RenderScene();
+	virtual void UpdateScene(float msec);
+
+protected:
+	void DrawHeightmap();
+	void DrawWater();
+	void DrawGrass(float xoff, float zoff);
+	void DrawSkybox();
+
+	Shader* lightShader;
+	Shader* reflectShader;
+	Shader*	grassShader;
+	Shader* skyboxShader;
+
+	HeightMap* heightMap;
+	Mesh*      quad;
+	Mesh*      quadGrass;
+	Light*     light;
+	Camera*    camera;
+
+	GLuint     cubeMap;
+	float      waterRotate;
+};
